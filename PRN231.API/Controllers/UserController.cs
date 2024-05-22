fa43ef9ccb4a67de55e4879d8d3fc0e1b90ccbec
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2024-05-22 15:56:49 +0700

    usercontroller and schedule controller

--- PRN231.API/Controllers/UserController.cs
+++ PRN231.API/Controllers/UserController.cs
@@ -10,6 +10,56 @@ namespace PRN231.API.Controllers
     public class UserController:ControllerBase
     {
         private readonly IGenericService<User, UserDTO> _userService;
+        private readonly ILogger<UserController> _logger;
+        public IConfiguration _configuration;
+
+        public UserController(IConfiguration config, ILogger<UserController> logger,
+                IGenericService<User, UserDTO> userService)
+        {
+            _logger = logger;
+            _configuration = config;
+            _userService = userService;
+        }
+
+        [HttpGet("GetAll")]
+        //[Authorize]
+        public async Task<IActionResult> GetAll()
+        {
+            var customerList = await _userService.GetAll();
+            return Ok(customerList);
+        }
+
+        [HttpGet("Get")]
+        //[Authorize]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var customerList = await _userService.Get(id);
+            return Ok(customerList);
+        }
+
+        [HttpPost("Add")]
+        //[Authorize]
+        public async Task<IActionResult> Add(UserDTO dto)
+        {
+            var brand = await _userService.Add(dto);
+            return Ok(brand);
+        }
+
+        [HttpPut("Update")]
+        //[Authorize]
+        public async Task<IActionResult> Update(UserDTO dto)
+        {
+            var brand = await _userService.Update(dto);
+            return Ok(brand);
+        }
+
+        [HttpDelete("Delete")]
+        //[Authorize]
+        public async Task<IActionResult> Delete([FromBody] Guid id)
+        {
+            var brand = await _userService.Delete(id);
+            return Ok(brand);
+        }
 
     }
 }
