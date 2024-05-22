1763843d23a39bce16f5cdd26b1dcc822ce1ca30
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2024-05-22 15:38:53 +0700

    add more DTOs

--- PRN231.Models/DTOs/FeedbackDTO.cs
+++ PRN231.Models/DTOs/FeedbackDTO.cs
@@ -1,11 +1,27 @@
+using PRN231.Models;
+
 namespace EXE101.Models.DTOs
 {
     public class FeedbackDTO
     {
-        public Guid? Id { get; set; } 
+        public int Id { get; set; }
 
-        public required string Message { get; set; }
+        public int TutorId { get; set; }
 
-        public required Guid ProductId { get; set; }
+        public int StudentId { get; set; }
+
+        public string Content { get; set; }
+
+        public int Rating { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public bool Status { get; set; }
+
+        public User Tutor { get; set; }
+
+        public User Student { get; set; }
     }
 }
--- PRN231.Models/DTOs/SubjectDTO.cs
+++ PRN231.Models/DTOs/SubjectDTO.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace PRN231.Models.DTOs
 {
-    internal class SubjectDTO
+    public class SubjectDTO
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Level { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public bool Status { get; set; }
+
     }
 }
