b8ef184a5c18dd3357d22f2be5d7a6d968a0148e
Minh Dinh <macotontai123@gmail.com>
2024-05-25 11:52:15 +0700

    feat(booking): add Create Booking api
    add MessageConstant and BookingStatusConstant

--- PRN231.API/Controllers/BookingController.cs
+++ PRN231.API/Controllers/BookingController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using PRN231.Constant;
 using PRN231.Models;
 using PRN231.Models.DTOs;
+using PRN231.Models.DTOs.Request;
 using PRN231.Models.DTOs.Response;
 using PRN231.Services;
 using PRN231.Services.Implementation;
@@ -27,9 +29,23 @@ namespace PRN231.API.Controllers
         {
             List<Booking> bookings = await _bookingService.GetAllBookings();
 
-            var response = new ApiResponse((int)HttpStatusCode.OK, "Get successfully", bookings);
+            var response = new ApiResponse((int)HttpStatusCode.OK, MessageConstant.SUCCESSFUL, bookings);
 
             return response;
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult<ApiResponse>> CreateBooking([FromBody] CreateBookingRequest request)
+        {
+            try
+            {
+                CreateBookingResponse bookingResponse = await _bookingService.CreateBooking(request);
+
+                return new ApiResponse((int)HttpStatusCode.OK, MessageConstant.SUCCESSFUL, bookingResponse);
+            } catch (Exception ex)
+            {
+                return new ApiResponse((int)HttpStatusCode.BadRequest, MessageConstant.FAILED, null);
+            }
         }
     }
 }
--- PRN231.API/JWTService.cs
+++ PRN231.API/JWTService.cs
@@ -5,7 +5,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace BirthdayParty.API
+namespace PRN231.API
 {
     public class JWTService
     {
