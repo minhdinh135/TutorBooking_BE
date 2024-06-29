e463470e4b0cb10f6ecb7b613f42d7e848fbe209
Trung Tuan <tuandtse172284@fpt.edu.vn>
2024-06-29 09:00:01 +0700

    fix conflict

---
+++ PRN231.DAL/Migrations/20240629014631_fix data feedback.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PRN231.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixdatafeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LevelName",
+                table: "Feedbacks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SubjectName",
+                table: "Feedbacks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LevelName",
+                table: "Feedbacks");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectName",
+                table: "Feedbacks");
+        }
+    }
+}
