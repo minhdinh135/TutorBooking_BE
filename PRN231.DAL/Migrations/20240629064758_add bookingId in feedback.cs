2458d79269043e0c0c162ac1171aa6fa1e8c3b1f
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2024-07-06 13:45:51 +0700

    ok

---
+++ PRN231.DAL/Migrations/20240629064758_add bookingId in feedback.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PRN231.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addbookingIdinfeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookingId",
+                table: "Feedbacks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookingId",
+                table: "Feedbacks");
+        }
+    }
+}
